7575393bc4502dce6ad4828232670229376be73e
Allon Mureinik <allon@synopsys.com>
2021-01-06 23:22:12 +0200

    Add RegExp benchmark

---
+++ benchmark/benchmark_regexp.js
@@ -0,0 +1,11 @@
+console.log('A\'s\ttime');
+let regex = new RegExp('^(a+)+$');
+
+for (let i = 1; i < 100; ++i) {
+    const str = Array(i + 1).join('a') + 'b';
+    const before = new Date();
+    regex.test(str);
+    const after = new Date();
+    const time = after - before;
+    console.log(`${i}\t${time}`);
+}
