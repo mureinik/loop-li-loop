d0397c51b300f1a22d2eb5359e3b705a3367ede1
Allon Mureinik <mureinik@gmail.com>
2025-11-28 17:16:36 +0200

    Add XML benchmark

---
+++ benchmark/benchmark_xml.js
@@ -0,0 +1,21 @@
+const libxmljs = require('libxmljs2');
+const lineSize = 10;
+const opts = {noent: true, nocdata: true, noblanks: true, huge: true};
+
+let xmlStart = `<?xml version="1.0"?>
+ <!DOCTYPE lolz [
+ <!ENTITY lol0 "lol">
+ <!ELEMENT lolz (#PCDATA)>`;
+
+console.log('#Lolz\tSize (MB)');
+
+const maxLolz = 9;
+for (let i = 1; i <= maxLolz; i++) {
+    const entityLine = `<!ENTITY lol${i} "${Array(lineSize).fill('&lol' + (i - 1) + ';').join('')}">`;
+    xmlStart += `\n ${entityLine}`;
+    const xml = `${xmlStart}\n]>\n<lolz>&lol${i};</lolz>`;
+
+    const parsed = libxmljs.parseXml(xml, opts);
+    const expandedLength = (parsed.root().text().length / 1024 / 1024).toFixed(4);
+    console.log(`${i}\t${expandedLength}`);
+}
